99a8fc1cc3d187ff3ff110b22ee35fdd8017dbef
Fernando R Gonzáalez <rumbaut2288@gmail.com>
2017-10-27 04:45:32 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,4 +1,10 @@
-var index = require("../index.js")
+var index = require("../index.js");
+
+describe('Name',()=>{
+  it('returns "Susan"',()=>{
+    expect(index.name).toEqual('Susan')
+  })
+})
 
 /*
 describe('what-is-a-test', () => {
